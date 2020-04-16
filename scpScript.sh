b86c1273023d25286ae3e6c754b0f60cf7a8b84f
Alfredo Oliveira <alfredo.coj@gmail.com>
2020-04-16 15:20:34 -0300

    5e989b4cd8c82910f58268c5 Integração de k8s do site 2 com Jenkins de produção - [02:00] - manifests de deploy do jenkins.

--- scpScript.sh
+++ scpScript.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-scp -r *.yaml k8s-admin@192.168.6.95:/home/k8s-admin/services/k8s-jenkins/
+#scp -r *.yaml k8s-admin@192.168.6.95:/home/k8s-admin/services/k8s-jenkins/
+scp -r *.yaml k8s-admin@192.168.254.12:/home/k8s-admin/services/k8s-jenkins/
+scp -r *.yaml k8s-admin@192.168.253.12:/home/k8s-admin/services/k8s-jenkins/
